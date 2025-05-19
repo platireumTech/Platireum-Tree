fcbd209670fbae3da802bea77aceac88480d0b68
Platireum <platireum@gmail.com>
2025-05-19 17:15:20 +0300

    Update ValueStabilizer.sol

--- ValueStabilizer.sol
+++ ValueStabilizer.sol
@@ -3,76 +3,70 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./Interfaces.sol";
 
 contract ValueStabilizer is Ownable {
-    uint256 public constant TARGET_PRICE = 1e18;
     uint256 public constant MAX_ADJUSTMENT = 0.10e18; // 10% max single adjustment
     uint256 public constant DEVIATION_THRESHOLD = 0.01e18; // 1%
 
     address public immutable mainToken;
     address public immutable assetManager;
-    address public immutable reserveToken;
+    address public immutable supplyController;
 
-    event SupplyAdjusted(int256 amount, uint256 assetValueMoved);
+    event SupplyAdjusted(int256 amount);
 
-    constructor(address _mainToken, address _assetManager, address _reserveToken) {
+    constructor(address _mainToken, address _assetManager, address _supplyController) {
         mainToken = _mainToken;
         assetManager = _assetManager;
-        reserveToken = _reserveToken;
+        supplyController = _supplyController;
     }
 
-    function adjustSupply(uint256 currentPrice) external {
-        require(msg.sender == owner() || msg.sender == mainToken, "Unauthorized");
+    function adjustSupplyBasedOnAssetValue() external {
+        require(msg.sender == supplyController, "Only SupplyController can call this");
 
-        uint256 deviation = _calculateDeviation(currentPrice);
-        if (deviation < DEVIATION_THRESHOLD) return;
+        uint256 currentAssetValueInGold = IAssetManager(assetManager).calculateAssetValueInGold();
+        uint256 totalSupply = IERC20(mainToken).totalSupply();
 
-        uint256 supplyChange = _calculateSupplyChange(deviation);
-        _executeSupplyAdjustment(supplyChange, currentPrice);
-    }
+        // Calculate the target price of the token in terms of our base unit (e.g., 1 token = X base units of gold value)
+        uint256 targetPricePerToken = totalSupply > 0 ? currentAssetValueInGold / totalSupply : 0;
+        uint256 currentMarketPricePerToken = ISupplyController(supplyController).getCurrentMarketPrice();
 
-    function _calculateDeviation(uint256 currentPrice) internal pure returns (uint256) {
-        return currentPrice > TARGET_PRICE
-            ? ((currentPrice - TARGET_PRICE) * 1e18) / TARGET_PRICE
-            : ((TARGET_PRICE - currentPrice) * 1e18) / TARGET_PRICE;
-    }
+        if (targetPricePerToken > 0 && currentMarketPricePerToken > 0) {
+            uint256 deviation = _calculateDeviation(currentMarketPricePerToken, targetPricePerToken);
 
-    function _calculateSupplyChange(uint256 deviation) internal view returns (uint256) {
-        uint256 tokenSupply = IERC20(mainToken).totalSupply();
-        uint256 proposedChange = (tokenSupply * deviation) / 1e18;
-
-        // Cap the adjustment
-        uint256 maxChange = (tokenSupply * MAX_ADJUSTMENT) / 1e18;
-        return proposedChange > maxChange ? maxChange : proposedChange;
-    }
-
-    function _executeSupplyAdjustment(uint256 supplyChange, uint256 currentPrice) internal {
-        if (currentPrice > TARGET_PRICE) {
-            _expandSupply(supplyChange);
-        } else {
-            _contractSupply(supplyChange);
+            if (deviation > DEVIATION_THRESHOLD) {
+                uint256 supplyChange = _calculateSupplyChange(deviation, totalSupply);
+                if (currentMarketPricePerToken > targetPricePerToken) {
+                    // Market price is too high, potentially issue more tokens
+                    ISupplyController(supplyController).mint(supplyChange);
+                    emit SupplyAdjusted(int256(supplyChange));
+                } else {
+                    // Market price is too low, potentially burn tokens
+                    ISupplyController(supplyController).burn(supplyChange);
+                    emit SupplyAdjusted(-int256(supplyChange));
+                }
+            }
         }
     }
 
-    function _expandSupply(uint256 mintAmount) internal {
-        require(
-            IAssetManager(assetManager).checkLiquidity(mintAmount * TARGET_PRICE / 1e18),
-            "Insufficient liquidity"
-        );
-
-        IMintable(mainToken).mint(address(this), mintAmount);
-        uint256 reserveAmount = (mintAmount * TARGET_PRICE) / 1e18;
-        IAssetManager(assetManager).buyAssetsProportionally(reserveAmount);
-
-        emit SupplyAdjusted(int256(mintAmount), reserveAmount);
+    function _calculateDeviation(uint256 currentPrice, uint256 targetPrice) internal pure returns (uint256) {
+        return currentPrice > targetPrice
+            ? ((currentPrice - targetPrice) * 1e18) / targetPrice
+            : ((targetPrice - currentPrice) * 1e18) / targetPrice;
     }
 
-    function _contractSupply(uint256 burnAmount) internal {
-        IBurnable(mainToken).burn(burnAmount);
-        uint256 reserveAmount = (burnAmount * TARGET_PRICE) / 1e18;
-        IAssetManager(assetManager).sellAssetsProportionally(reserveAmount);
-
-        emit SupplyAdjusted(-int256(burnAmount), reserveAmount);
+    function _calculateSupplyChange(uint256 deviation, uint256 totalSupply) internal view returns (uint256) {
+        uint256 proposedChange = (totalSupply * deviation) / 1e18;
+        uint256 maxChange = (totalSupply * MAX_ADJUSTMENT) / 1e18;
+        return proposedChange > maxChange ? maxChange : proposedChange;
     }
+}
+
+interface IAssetManager {
+    function calculateAssetValueInGold() external view returns (uint256);
+}
+
+interface ISupplyController {
+    function getCurrentMarketPrice() external view returns (uint256);
+    function mint(uint256 amount) external;
+    function burn(uint256 amount) external;
 }
