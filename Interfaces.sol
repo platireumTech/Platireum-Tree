fe12462de5dcac47a461508e18a6981b1aeab51b
Platireum <platireum@gmail.com>
2025-05-19 17:18:00 +0300

    Update Interfaces.sol

--- Interfaces.sol
+++ Interfaces.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface IAssetManager {
+    function calculateAssetValueInGold() external view returns (uint256);
+    function getGoldPriceUSD() external view returns (uint256);
+}
+
+interface IValueStabilizer {
+    function adjustSupplyBasedOnAssetValue() external;
+}
+
+interface ISupplyController {
+    function getCurrentMarketPrice() external view returns (uint256);
+    function mint(uint256 amount) external;
+    function burn(uint256 amount) external;
+}
+
 interface IMintable {
     function mint(address to, uint256 amount) external;
 }
 
 interface IBurnable {
-    function burn(uint256 amount) external;
-}
-
-interface IAssetManager {
-    function buyAssetsProportionally(uint256 reserveAmount) external;
-    function sellAssetsProportionally(uint256 reserveAmount) external;
-    function getTotalAssetValue() external view returns (uint256);
-    function checkLiquidity(uint256 usdAmount) external view returns (bool);
+    function burn(address from, uint256 amount) external;
 }
